04c82a615de57773329b4b5131243f931aee5732
MATSUOKA Kohei <kohei@machu.jp>
2018-01-04 23:21:08 +0900

    チャンネル変更時の処理を改善

--- bravia/bravia.js
+++ bravia/bravia.js
@@ -15,6 +15,19 @@ bravia('192.168.1.47', 'empicnrxmp', (client) => {
       console.log(`Exec ${command}`);
       client.exec(command);
     }
+  } else if (command.startsWith('Num')) {
+    // Alexeで1%が100%になってしまう仕様回避
+    if (command == "Num100") {
+      command = "Num1";
+    }
+    // hdmi選択時でもテレビに切り替え
+    console.log(`Exec Tv`);
+    client.exec('Tv');
+    // 500ミリ秒あけないと次の命令を受け付けない
+    setTimeout(() => {
+      console.log(`Exec ${command}`);
+      client.exec(command);
+    }, 500);
   } else {
     console.log(`Exec ${command}`);
     client.exec(command);
